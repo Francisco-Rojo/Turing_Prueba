8d72c8f01640e443d31604844420abf32458bc4a
Francisco-Rojo-C <59403516+Francisco-Rojo@users.noreply.github.com>
2022-12-02 10:46:23 -0600

    content main - home
    video y contenido principal

--- includes/header.php
+++ includes/header.php
@@ -5,28 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-
-    <style>
-        #container-home {
-            background-color: black;
-            color: white;
-            height: 40vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-        footer {
-          background-color: #002faf;
-        }
-    </style>
-
+    <link rel="stylesheet" href="./css/styles.css">
+        <link rel="stylesheet" href="./css/bootstrap.min.css">
 </head>
 <body>
     
 <header class="fixed-top">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
-      
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">  
     <div class="container">
         <a class="navbar-brand" href="#"><h1>TURING-IA</h1></a>
         <button class="navbar-toggler navbar-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
--- index.php
+++ index.php
@@ -2,25 +2,51 @@
     include("includes/header.php");
 ?>
 
-<div id="container-home">
-    
-    <div class="container mt-5 text-center">
-        <div class="row">
-            <div class="col-12">
-                <div class="">
 
-                    <h1 class="display-1">Bienvenido</h1>
-                    <p>Hacemos los datos fáciles de usar</p>
-                </div>
-    
+
+
+<div class="container_home">
+    <div class="container_video">
+        <video autoplay muted loop>
+            <source src="./videos/Index.m4v" type="video/mp4">
+        </video>
+    </div>
+
+    <div class="container_home_content"">
+        <div class=" container">
+        <div class="row">
+
+            <div class="col-12">
+
+                <h1 class="text-center text-lg-start  fw-bolder display-3">Bienvenido</h1>
+                <p class="h3 text-center text-lg-start fw-bolder ">Hacemos los datos <br>fáciles de usar</p>
             </div>
         </div>
     </div>
 </div>
 
 
+</div>
 
-<section  class="container mt-5">
+
+<!-- 
+<div id="container-home">
+    <div class="container mt-5 text-center">
+        <div class="row">
+     
+            <div class="col-12">
+                <div class="">
+                    <h1 class="display-1">Bienvenido</h1>
+                    <p>Hacemos los datos fáciles de usar</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> -->
+
+
+
+<section class="container mt-5">
     <article class="row">
         <div class="col-12">
             <h2>Que Realizamos</h2>
@@ -36,7 +62,7 @@
     <article>
         <!-- carrusel de contenido -->
     </article>
-    
+
     <article class="row text-center mt-3">
         <!-- Tecnologias que se utilizan -->
         <div class="row">
@@ -46,16 +72,16 @@
         </div>
 
         <div class="col-12">
-            <img class="img-fluid" width="150px"  src="img/tecnoly-tableau.png" alt="tableu">
-        </div> 
+            <img class="img-fluid" width="150px" src="img/tecnoly-tableau.png" alt="tableu">
+        </div>
         <div cla ss="col-12">
-            <img class="img-fluid" width="150px"  src="img/salesforce.jpg" alt="salesforce">
-        </div> 
+            <img class="img-fluid" width="150px" src="img/salesforce.jpg" alt="salesforce">
+        </div>
         <div cla ss="col-12">
-            <img  class="img-fluid" width="150px" src="img/pmi2.png" alt="project managment">
-        </div> 
+            <img class="img-fluid" width="150px" src="img/pmi2.png" alt="project managment">
+        </div>
         <div cla ss="col-12">
-            <img class="img-fluid" width="150px"  src="img/scrum.png" alt="scrum">
+            <img class="img-fluid" width="150px" src="img/scrum.png" alt="scrum">
         </div>
 
     </article>
