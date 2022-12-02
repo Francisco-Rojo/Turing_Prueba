8be82fc59d5ed9cc8afb49a7c23d844006587540
Francisco-Rojo-C <59403516+franciscoDiablo@users.noreply.github.com>
2022-12-01 18:57:36 -0600

    update menu

--- includes/header.php
+++ includes/header.php
@@ -21,22 +21,23 @@
 </head>
 <body>
     
-<header class="">
+<header class="fixed-top ">
     <nav class="navbar navbar-expand-lg ">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#"><h1>TURING-IA</h1></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <a class="navbar-brand" href="#"><h1 class="text-white">TURING-IA</h1></a>
+        <button class="navbar-toggler navbar-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon">
+          <i class="fa fa-navicon" style="color:#fff; font-size:28px;"></i>
+          </span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-            <a class="nav-link" href="#">Features</a>
-            <a class="nav-link" href="#">Pricing</a>
-            <a class="nav-link disabled">Disabled</a>
+            <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+            <a class="nav-link text-white" href="#">Features</a>
+            <a class="nav-link text-white" href="#">Pricing</a>
+            <a class="nav-link disabled text-white">Disabled</a>
           </div>
         </div>
       </div>
     </nav>
 </header>
-<br>
