9a811de5daf908062a9a99516c918c08e7b8128d
Francisco-Rojo-C <59403516+Francisco-Rojo@users.noreply.github.com>
2022-12-02 12:16:56 -0600

    content-video-home

--- index.php
+++ index.php
@@ -14,42 +14,38 @@
 
     <div class="container_home_content"">
         <div class=" container">
-        <div class="row">
-
-            <div class="col-12">
-
-                <h1 class="text-center text-lg-start  fw-bolder display-3">Bienvenido</h1>
-                <p class="h3 text-center text-lg-start fw-bolder ">Hacemos los datos <br>fáciles de usar</p>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-</div>
-
-
-<!-- 
-<div id="container-home">
-    <div class="container mt-5 text-center">
-        <div class="row">
-     
-            <div class="col-12">
-                <div class="">
-                    <h1 class="display-1">Bienvenido</h1>
-                    <p>Hacemos los datos fáciles de usar</p>
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-center text-lg-start  fw-bolder display-3">Bienvenido</h1>
+                    <p class="h3 text-center text-lg-start fw-bolder ">Hacemos los datos <br>fáciles de usar</p>
                 </div>
             </div>
+            <div class="row mt-4">
+                <div class="col-12">
+                    <p class="h4 text-center text-lg-start ">Ponte en contacto con un especialiasta</p>
+                    <div class="d-flex justify-content-center justify-content-lg-start">
+                        <button type="button" class="btn btn-outline-light rounded-0 ">
+                            Más Información
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+
+
         </div>
     </div>
-</div> -->
+</div>
 
+<div class="container-fluid">
 
+    <img width="100%" src="./img/color-bar-w-blue-3010x50.png" alt="bar-color">
+</div>
 
 <section class="container mt-5">
     <article class="row">
         <div class="col-12">
-            <h2>Que Realizamos</h2>
+            <h2 class="text-uppercase">Que Realizamos</h2>
             <p>Contribuimos con su empresa para mejorar la toma de
                 decisiones a través de sus datos. </p>
 
@@ -63,27 +59,29 @@
         <!-- carrusel de contenido -->
     </article>
 
-    <article class="row text-center mt-3">
-        <!-- Tecnologias que se utilizan -->
-        <div class="row">
-            <div class="col-12 text-center">
-                <h2>Tecnologias que utilizamos</h2>
+
+    
+
+    <article class="row text-center">
+
+        <div class="row text-center mt-4 mb-2">
+            <div class="col-12">
+                <h2 class="text-uppercase">Tecnologias que utilizamos</h2>
             </div>
         </div>
 
-        <div class="col-12">
+        <div class="col-12 col-lg-3">
             <img class="img-fluid" width="150px" src="img/tecnoly-tableau.png" alt="tableu">
         </div>
-        <div cla ss="col-12">
+        <div class="col-12 col-lg-3">
             <img class="img-fluid" width="150px" src="img/salesforce.jpg" alt="salesforce">
         </div>
-        <div cla ss="col-12">
+        <div class="col-12 col-lg-3">
             <img class="img-fluid" width="150px" src="img/pmi2.png" alt="project managment">
         </div>
-        <div cla ss="col-12">
+        <div class="col-12 col-lg-3">
             <img class="img-fluid" width="150px" src="img/scrum.png" alt="scrum">
         </div>
-
     </article>
 
 
