94a553a207aedea833a6de15163c15cd40c93061
Francisco-Rojo-C <59403516+Francisco-Rojo@users.noreply.github.com>
2022-12-05 18:56:02 -0600

    paginas header
    pagina nosotros, blog header

---
+++ blog.php
@@ -0,0 +1,39 @@
+<?php 
+    include("includes/header.php");
+?>
+
+
+<div class="container_home">
+    <div class="container_video">
+        <video autoplay muted loop>
+            <source  src="./videos/Blog.mp4" type="video/mp4">
+        </video>
+    </div>
+
+    <div class="container_home_content"">
+        <div class=" container">
+        <div class="row">
+            <div class="col-3">
+                <!-- Se requiere para activar un mensaje cuando se envia datos desde el form -->
+                <?php  if(isset($_SESSION['message'])) { ?>
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    <?= $_SESSION['message'] ?>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                <?php session_unset(); } ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center text-lg-start  fw-bolder display-3">Blog</h1>
+                <p class="h3 text-center text-lg-start fw-bolder">
+                    Los grandes conocimientos <br>egendran las grandes <b>dudas</p>
+            </div>
+        </div>
+       
+    </div>
+</div>
+</div>
+<div class="container-fluid">
+    <img class="w-100" src="./img/color-bar-w-blue-3010x50.png" alt="bar-color">
+</div>
--- includes/header.php
+++ includes/header.php
@@ -16,7 +16,7 @@
 <header class="fixed-top">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">  
     <div class="container">
-        <a class="navbar-brand" href="#"><h1 class="text-uppercase fw-light">TURING-IA</h1></a>
+        <a class="navbar-brand" href="index.php"><h1 class="text-uppercase fw-light">TURING-IA</h1></a>
         <button class="navbar-toggler navbar-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon">
           <i class="fa fa-navicon" style="color:#fff; font-size:28px;"></i>
@@ -24,10 +24,13 @@
         </button>
         <div class="collapse navbar-collapse d-lg-flex justify-content-md-end" id="navbarNavAltMarkup">
           <div class="navbar-nav text-center">
-            <a class="nav-link active " aria-current="page" href="#">Home</a>
-            <a class="nav-link " href="#">Features</a>
-            <a class="nav-link " href="#">Pricing</a>
-            <a class="nav-link disabled text-white">Disabled</a>
+            <a class="nav-link " aria-current="page" href="index.php">Inicio</a>
+            <a class="nav-link " href="nosotros.php">Nosotros</a>
+            <a class="nav-link " href="#">Servicios</a>
+            <a class="nav-link " href="#">Tableau</a>
+            <a class="nav-link " href="#">Portal</a>
+            <a class="nav-link " href="blog.php">Blog</a>
+            <a class="nav-link btnModal" href="#">Contacto</a>
           </div>
         </div>
       </div>
